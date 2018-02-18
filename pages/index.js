7ac865cbe5d8c15d14dab1ba95e54abc6c11271c
Detik <the2ndflamingheart@gmail.com>
2018-02-19 03:20:13 +0700

    feat(pages): Add posts and albums user link in Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import { Table } from 'semantic-ui-react';
+import { Divider, Table } from 'semantic-ui-react';
+import URI from 'urijs';
 import PageHeader from '../components/PageHeader';
 import Layout from '../components/Layout';
 import TableContent from '../components/table/ColumnContent';
+import LinkButton from '../components/link/Button';
 import fetch from '../lib/fetch';
 
 const Homepage = ({ users }) => (
@@ -17,50 +19,66 @@ const Homepage = ({ users }) => (
           <Table.HeaderCell>Phone</Table.HeaderCell>
           <Table.HeaderCell>Website</Table.HeaderCell>
           <Table.HeaderCell>Company</Table.HeaderCell>
+          <Table.HeaderCell>Action</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
 
       <Table.Body>
         {users.map(({
- id, name, username, email, phone, website, company,
-}) => (
-  <Table.Row key={id}>
-    <Table.Cell>
-      <TableContent>
-        {id}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent>
-        {name}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent>
-        {username}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent>
-        {email}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent>
-        {phone}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent link={`http://www.${website}`}>
-        {website}
-      </TableContent>
-    </Table.Cell>
-    <Table.Cell>
-      <TableContent>
-        {company.name}
-      </TableContent>
-    </Table.Cell>
-  </Table.Row>
+          id,
+          name,
+          username,
+          email,
+          phone,
+          website,
+          company,
+        }) => (
+          <Table.Row key={id}>
+            <Table.Cell>
+              <TableContent>
+                {id}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent>
+                {name}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent>
+                {username}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent>
+                {email}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent>
+                {phone}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent link={`http://www.${website}`}>
+                {website}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <TableContent>
+                {company.name}
+              </TableContent>
+            </Table.Cell>
+            <Table.Cell>
+              <LinkButton href={new URI('/posts').addSearch({ userId: id }).toString()} color="green">
+                {"User's Posts"}
+              </LinkButton>
+              <Divider horizontal />
+              <LinkButton href={new URI('/albums').addSearch({ userId: id }).toString()} color="green">
+                {"User's Albums"}
+              </LinkButton>
+            </Table.Cell>
+          </Table.Row>
         ))}
       </Table.Body>
     </Table>
