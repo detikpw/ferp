30cbf1e76f77822246a6497541a92b626823aef1
Detik <the2ndflamingheart@gmail.com>
2018-02-19 05:06:20 +0700

    feat(component): Handle alias in LinkButton component

--- components/link/Button.js
+++ components/link/Button.js
@@ -1,9 +1,14 @@
 import { Button } from 'semantic-ui-react';
 import Link from 'next/link';
 
-export default ({ href, children, ...other }) => (
+export default ({
+  alias,
+  href,
+  children,
+  ...other
+}) => (
   <Button {...other}>
-    <Link href={href}>
+    <Link href={href} as={alias}>
       <a style={{ color: '#fff' }}>{children}</a>
     </Link>
   </Button>
