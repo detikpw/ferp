36a658deff238f83406fc0bb969dcbd3b5063624
Detik <the2ndflamingheart@gmail.com>
2018-02-19 06:22:15 +0700

    feat(pages): Add list of comments in Post detail page

--- constants/site-config.js
+++ constants/site-config.js
@@ -1,3 +1,3 @@
 export const SERVER_API_URL = 'https://jsonplaceholder.typicode.com/';
 export const TIMEOUT = 100000;
-export const DEFAULT_AVATAR = "http://uc48.net/europeana/images/fpo_avatar.png";
+export const DEFAULT_AVATAR = 'http://uc48.net/europeana/images/fpo_avatar.png';
--- pages/post.js
+++ pages/post.js
@@ -1,13 +1,39 @@
-import { Segment } from 'semantic-ui-react';
+import { Comment, Header, Segment } from 'semantic-ui-react';
+import URI from 'urijs';
 import PageHeader from '../components/PageHeader';
 import Layout from '../components/Layout';
 import fetch from '../lib/fetch';
+import { DEFAULT_AVATAR } from '../constants/site-config';
 
-const Post = ({ post, user }) => (
+const Post = ({ comments, post, user }) => (
   <Layout>
     <Segment raised>
       <PageHeader icon="sticky note">{post.title} by {user.name}</PageHeader>
       {post.body}
+    </Segment>
+    <Segment raised>
+      <Comment.Group>
+        <Header as="h3" dividing>Comments</Header>
+        { comments.map(({
+          id,
+          email,
+          name,
+          body,
+        }) => (
+          <Comment key={id}>
+            <Comment.Avatar src={DEFAULT_AVATAR} />
+            <Comment.Content>
+              <Comment.Author>
+                {name}
+              </Comment.Author>
+              <Comment.Author>
+                {email}
+              </Comment.Author>
+              <Comment.Text>{body}</Comment.Text>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
     </Segment>
   </Layout>
 );
@@ -16,10 +42,13 @@ Post.getInitialProps = async (context) => {
   const { id } = context.query;
   const postRes = await fetch(`/posts/${id}`);
   const post = await postRes.data;
+  const commentsRes = await fetch(new URI('/comments').addSearch({ postId: id }).toString());
+  const comments = await commentsRes.data;
   const userRes = await fetch(`users/${post.userId}`);
   const user = await userRes.data;
 
   return {
+    comments,
     post,
     user,
   };
